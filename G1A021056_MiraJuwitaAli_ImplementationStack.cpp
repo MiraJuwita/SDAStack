e8019a4f28f99af68ea3082d745f7ad194575e6e
MiraJuwita <98795964+MiraJuwita@users.noreply.github.com>
2022-06-06 00:05:28 +0700

    Add files via upload

---
+++ G1A021056_MiraJuwitaAli_ImplementationStack.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct Stack {
+    int top; 
+	int data[MAX];
+} S;
+
+void init() {
+    S.top = -1;
+}
+ 
+bool isEmpty() {
+    return S.top == -1;
+}
+ 
+bool isFull() {
+    return S.top == MAX -1;
+}
+ 
+void push() {
+    if (isFull()) {
+        cout << "\nStack Penuh" << endl;
+    } else {
+        S.top++;
+        cout << "\nMasukkan data : ";
+        cin >> S.data[S.top];
+        cout << "\n Data : '" << S.data[S.top] <<endl; 
+		cout << "Data telah ditambahkan"<< endl;
+    }
+}
+ 
+void pop() {
+    if (isEmpty()) {
+        cout << "\nStack kosong " << endl;
+    } else {
+        cout << "\n Data :"<<S.data[S.top] <<endl;
+        cout << "Data Dihapus" << endl;
+        S.top--;
+    }
+}
+ 
+void printStack() {
+    if (isEmpty()) {
+        cout<< "\nStack Kosong\n";
+    } else {
+        cout << "Data Stack : ";
+        for (int i = S.top; i >= 0; i--)
+            cout << S.data[i] <<  ( ",");
+    }
+}
+
+int main() {
+	int choose;
+	do
+	{
+		cout << "MenuStack\n"
+			<< " 1. Menambahkan Data  \n"
+			<< " 2. Menghapus Data \n"
+			<< " 3. Menampilkan Data \n"
+			<< " 4. EXIT \n\n"
+			<< "Masukkan pilihan : "; cin >> choose;
+		switch (choose)
+		{
+		case 1:
+			push();
+			break;
+		case 2:
+			pop();
+			break;
+		case 3:
+			printStack();
+			break;
+		default:
+			cout << "Pilihan tidak ada";
+			break;
+		}
+	} while (choose !=3);
+	return 0;
+}
